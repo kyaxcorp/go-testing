74972f9702d6bb21f32da3560c698239dcbd8872
Tabuci Octavian <octavian@tabuci.org>
2022-07-10 15:28:18 +0300

    channels

---
+++ examples/chan/read8/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"log"
+	"time"
+)
+
+// when sending, the goroutine that is sending will be blocked until the reader will select!
+// if even multiple goroutines are sending, they will be waiting in a queue until the reader will get to it...
+// FIFO
+// multiple readers and writers
+func main() {
+	chan1 := make(chan bool)
+
+	go func() {
+		for {
+			chan1 <- true
+			log.Println("go 1 -> sent a message!")
+			//time.Sleep(time.Second)
+		}
+	}()
+	go func() {
+		for {
+			chan1 <- true
+			log.Println("go 2 -> sent a message!")
+			//time.Sleep(time.Second)
+		}
+	}()
+
+	go func() {
+		for {
+
+			time.Sleep(time.Second * 5)
+			// let's print the value
+
+			select {
+			case chanVal := <-chan1:
+				log.Println("1 - reading message", chanVal)
+			}
+		}
+	}()
+	go func() {
+		for {
+
+			time.Sleep(time.Second * 5)
+			// let's print the value
+
+			select {
+			case chanVal := <-chan1:
+				log.Println("2 - reading message", chanVal)
+			}
+		}
+	}()
+
+	for {
+
+	}
+	// if using select instead of for, you will receive an error
+	// that all goroutines are asleep!
+	//select {}
+}
