df65975a5c59b466bd00cfc116aad6d8edd4ca4a
Tabuci Octavian <octavian@tabuci.org>
2022-07-11 06:32:35 +0300

    channels

--- examples/chan/read9/main.go
+++ examples/chan/read9/main.go
@@ -2,21 +2,21 @@ package main
 
 import (
 	"log"
-	"time"
 )
 
 // in this case we cannot read a value from a channel that hasn't any value pushed to it
 func main() {
 	chan1 := make(chan bool)
-	chan1 <- true
+	go func() {
+		chan1 <- true
+	}()
 
 	log.Println("everything set!")
 	go func() {
-		for {
-
-			time.Sleep(time.Second)
+		select {
+		case val := <-chan1:
 			// let's print the value
-			log.Println(<-chan1)
+			log.Println(val)
 		}
 	}()
 	//select {}
