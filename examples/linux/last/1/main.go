31dd7e1e06c5a000890b9b45a6b2eda140d4f42d
Tabuci Octavian <octavian@tabuci.org>
2022-07-24 23:26:32 +0300

    last command

--- examples/linux/last/1/main.go
+++ examples/linux/last/1/main.go
@@ -7,9 +7,11 @@ import (
 )
 
 func main() {
-	out, err := exec.CommandContext(context.Background(), "who").Output()
+	out, err := exec.CommandContext(context.Background(), "last").Output()
+	// TODO: check by still logged in
 	if err != nil {
 		log.Fatal(err)
 	}
 	log.Println("who is logged in", string(out))
+	// we need to see the current active users...
 }
---
+++ examples/linux/who/2/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os/exec"
+)
+
+func main() {
+	out, err := exec.CommandContext(context.Background(), "who").Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("who is logged in", string(out))
+	// we need to see the current active users...
+}
