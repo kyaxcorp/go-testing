a906a1fcf5cc4ce01269d934099050ac5683cc67
Tabuci Octavian <octavian@tabuci.org>
2022-07-25 07:19:59 +0300

    who cmd

--- examples/linux/who/2/main.go
+++ examples/linux/who/2/main.go
@@ -7,10 +7,14 @@ import (
 )
 
 func main() {
-	out, err := exec.CommandContext(context.Background(), "who").Output()
+	// -u --users  list users logged in
+	// -H --heading  print line of column headings
+	out, err := exec.CommandContext(context.Background(), "who", "-u", "-H").Output()
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Println("who is logged in", string(out))
+	log.Println("who is logged in")
+	log.Println(string(out))
+
 	// we need to see the current active users...
 }
