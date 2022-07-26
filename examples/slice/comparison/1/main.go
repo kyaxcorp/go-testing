c7fb6f74365e83d1695fde84b74e910f57dde7ef
Tabuci Octavian <octavian@tabuci.org>
2022-07-26 18:00:13 +0300

    reflection

---
+++ examples/slice/comparison/1/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"reflect"
+)
+
+func main() {
+	slice1 := []string{"jordan", "wise"}
+	slice2 := []string{"jordan", "wise", "junior"}
+
+	/*if slice1 == slice2 {
+
+	}*/
+
+	if reflect.DeepEqual(slice1, slice2) {
+		log.Println("are equal")
+	} else {
+		log.Println("aren't equal")
+	}
+
+}
